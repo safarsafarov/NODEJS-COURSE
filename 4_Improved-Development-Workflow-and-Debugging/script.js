1a5da8f9f3d579e86722fef32f4d24cb12ad7223
safarovsafar <safarov7safar@gmail.com>
2019-11-16 13:51:50 +0800

    go

---
+++ 4_Improved-Development-Workflow-and-Debugging/script.js
@@ -0,0 +1,43 @@
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+ const url = req.url;
+ const method = req.method;
+ if (url === '/') {
+  res.write('<html>');
+  res.write('<head><title>Enter Message</title><head>');
+  res.write('<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>');
+  res.write('</html>');
+  return res.end();
+ }
+ if (url === '/message' && method === 'POST') {
+  const body = [];
+  res.on('data', () => {
+   console.log(chunk);
+   body.push();
+  });
+  req.on('end', () => {
+   const pareseBody = Buffer.concat(body).toString;
+   const messages = pareseBody.split('=')[0];
+   fs.writeFile('message.txt', message, err => {
+    res.statusCode = 302;
+    res.setHeader('Locatuon', '/');
+    return res.end();
+   });
+   console.log(pareseBody);
+   res.statusCode = 302;
+   res.setHeader('Location', '/');
+   return res.end();
+
+  });
+ }
+ res.setHeader('Content-type', 'text/html');
+ res.write('<html>');
+ res.write('<head><title>My first Page</title><head>');
+ res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+ res.write('</html>');
+ res.end();
+});
+
+server.listen(3000);
