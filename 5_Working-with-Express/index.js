abfb0a39ae56dcdf92ce167ac0a21fdf77038ae8
safarovsafar <safarov.safar@hotmail.com>
2019-11-27 00:25:13 +0800

    go

--- 5_Working-with-Express/index.js
+++ 5_Working-with-Express/index.js
@@ -3,10 +3,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res, next) => {
- console.log('In the middleware! ');
+app.use((res, req, next) => {
+ console.log('In the middleware');
+ next();
 });
 
+app.use((req, res, next) => {
+ console.log('In another middlware!');
+ res.send();
+});
 
 const server = http.createServer(app);
+
 server.listen(3000);
