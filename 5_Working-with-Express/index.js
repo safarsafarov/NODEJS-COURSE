bea6fddc07e5730cd1627670a886624d5a94ec1c
safarovsafar <safarov.safar@hotmail.com>
2019-11-28 01:28:00 +0800

    'fo'

--- 5_Working-with-Express/index.js
+++ 5_Working-with-Express/index.js
@@ -2,14 +2,9 @@ const express = require('express');
 
 const app = express();
 
-app.use((res, req, next) => {
- console.log('In the middleware');
- next(); // Allows the request to continue to the next middleware in line
-});
-
 app.use((req, res, next) => {
- console.log('In another middlware!');
- res.send('<h1>Hello from Express!</h1>');
+  console.log('In another middlware!');
+  res.send('<h1>Hello from Express!</h1>');
 });
 
 app.listen(3000);
