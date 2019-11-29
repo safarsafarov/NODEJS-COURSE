a6c5f95098f283ae83b3b84e8310682400b92464
safarovsafar <safarov.safar@hotmail.com>
2019-11-30 07:52:18 +0800

    go

--- 5_Working-with-Express/index.js
+++ 5_Working-with-Express/index.js
@@ -2,8 +2,18 @@ const express = require('express');
 
 const app = express();
 
+app.use('/', (req, res, next) => {
+  console.log('This awlys runs!');
+  next();
+});
+
 app.use('/add-product', (req, res, next) => {
-  console.log('In another middlware!');
+  console.log('In another middleware!');
+  res.send('<h1>The "Add Product" Page</h1>');
+});
+
+app.use('/', (req, res, next) => {
+  console.log('In another middleware!');
   res.send('<h1>Hello from Express!</h1>');
 });
 
