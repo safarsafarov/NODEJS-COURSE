7c4bcec25637be7018b592da2b51f6a0eba0dbb5
safarovsafar <safarov.safar@hotmail.com>
2019-12-02 02:17:55 +0800

    go

--- 5_Working-with-Express/index.js
+++ 5_Working-with-Express/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const app = express();
 
+
 app.use(bodyPareser.urlencoded({extended: false}));
 
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
--- 5_Working-with-Express/routes/admin.js
+++ 5_Working-with-Express/routes/admin.js
@@ -2,11 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-router.get('/add-product', (req, res, next) => {
- res.send('<form action="/product> method="POST"><input type="text" name="title<button type="submit">"');
+// /admin/add-product => GET
+router.get('/admin/add-product', (req, res, next) => {
+ res.send('<form action="/admin/product> method="POST"><input type="text" name="title<button type="submit">"');
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/add=product => POST
+router.post('/admin/product', (req, res, next) => {
  console.log(req.body);
  res.redirect('/');
 });
