381eb52a551c79f868d1aca8f29a7be398f4e3f5
safarovsafar <safarov.safar@hotmail.com>
2019-12-11 18:23:51 +0800

    go

--- 5_Working-with-Express/app.js
+++ 5_Working-with-Express/app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -8,6 +9,7 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
