8c20462101003ca9a21ccee4d480463d899c5f93
safarovsafar <safarov.safar@hotmail.com>
2020-01-13 20:28:38 +0800

    go

--- 5_Working-with-Express/app.js
+++ 5_Working-with-Express/app.js
@@ -5,6 +5,8 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
